53dfb393d83dfe59ffef27ba2f4d412b8d400267
Johan Lohi <t5lojo00@students.oamk.fi>
2018-04-18 13:23:19 +0300

    Initial commit

---
+++ desktop/src/com/matiikka/harj/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.matiikka.harj.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.matiikka.harj.MyVectorGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyVectorGame(), config);
+	}
+}
